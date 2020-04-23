c8a8cd8f052d78d05ad20ec85c553e7ce0bf93c7
Pablo Bagliere <pablo.bagliere2@gmail.com>
2020-04-23 13:38:48 -0300

    Fix: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import rolRauter from "./routes/rol.router";
+import cors from "cors";
 const app: Application = express();
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
@@ -7,6 +8,7 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
 });
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/roles", rolRauter)
 
--- src/routes/index.router.ts
+++
@@ -1,8 +0,0 @@
-import app from "../app";
-import rolRouter from "./rol.router";
-
-// Location of all routes
-
-export const Routers = () => {
-  app.use("/roles", rolRouter);
-};
